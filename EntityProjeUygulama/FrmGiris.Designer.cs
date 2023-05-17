b7f12e354dcc773c5ec105f428faed053017d0ad
SerkanOzturkk <121403867+SerkanOzturkk@users.noreply.github.com>
2023-05-17 19:11:02 +0300

    Add project files.

---
+++ EntityProjeUygulama/FrmGiris.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace EntityProjeUygulama
+{
+    partial class FrmGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtKullanici = new System.Windows.Forms.TextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.BtnGirisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullancı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre:";
+            // 
+            // TxtKullanici
+            // 
+            this.TxtKullanici.Location = new System.Drawing.Point(131, 47);
+            this.TxtKullanici.Name = "TxtKullanici";
+            this.TxtKullanici.Size = new System.Drawing.Size(190, 26);
+            this.TxtKullanici.TabIndex = 2;
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(131, 79);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(190, 26);
+            this.TxtSifre.TabIndex = 3;
+            this.TxtSifre.UseSystemPasswordChar = true;
+            // 
+            // BtnGirisYap
+            // 
+            this.BtnGirisYap.Location = new System.Drawing.Point(159, 111);
+            this.BtnGirisYap.Name = "BtnGirisYap";
+            this.BtnGirisYap.Size = new System.Drawing.Size(124, 28);
+            this.BtnGirisYap.TabIndex = 4;
+            this.BtnGirisYap.Text = "Giriş Yap";
+            this.BtnGirisYap.UseVisualStyleBackColor = true;
+            this.BtnGirisYap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FrmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 174);
+            this.Controls.Add(this.BtnGirisYap);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.TxtKullanici);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtKullanici;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Button BtnGirisYap;
+    }
+}
---
+++ EntityProjeUygulama/FrmUrun.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityProjeUygulama
+{
+    public partial class FrmUrun : Form
+    {
+        public FrmUrun()
+        {
+            InitializeComponent();
+        }
+
+        DbEntityUrunEntities db = new DbEntityUrunEntities();
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = (from x in db.TBLURUN
+                                        select new 
+                                        { x.URUNID,
+                                          x.URUNAD,
+                                          x.MARKA, 
+                                          x.STOK, 
+                                          x.FIYAT,
+                                          x.TBLKATEGORI.AD,
+                                          x.DURUM
+                                        }).ToList();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            TBLURUN t = new TBLURUN();
+            t.URUNAD = TxtUrunAdı.Text;
+            t.MARKA = TxtMarka.Text;
+            t.STOK = short.Parse(TxtStok.Text);
+            t.FIYAT = decimal.Parse(TxtFiyat.Text);
+            t.DURUM = true;
+            t.KATEGORI = int.Parse(CmbKategori.Text);
+            db.TBLURUN.Add(t);
+            db.SaveChanges();
+            MessageBox.Show("Ürün Eklendi");
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(TxtID.Text);
+            var urunid = db.TBLURUN.Find(x);
+            db.TBLURUN.Remove(urunid);
+            db.SaveChanges();
+            MessageBox.Show("Ürün Silindi");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            TxtID.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            TxtUrunAdı.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            TxtMarka.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            TxtStok.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            TxtFiyat.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            TxtDurum.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            CmbKategori.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(TxtID.Text);
+            var urunid = db.TBLURUN.Find(x);
+            urunid.URUNAD = TxtUrunAdı.Text;
+            urunid.MARKA = TxtMarka.Text;
+            urunid.STOK = short.Parse(TxtStok.Text);
+            urunid.FIYAT = decimal.Parse(TxtFiyat.Text);
+            MessageBox.Show("Ürün Güncellendi");
+        }
+
+        private void FrmUrun_Load(object sender, EventArgs e)
+        {
+            var kategoriler = (from x in db.TBLKATEGORI
+                               select new
+                               {
+                                   x.ID,
+                                   x.AD
+                               }
+                               ).ToList();
+            CmbKategori.ValueMember = "ID";
+            CmbKategori.DisplayMember = "AD";
+            CmbKategori.DataSource = kategoriler;
+        }
+    }
+}
